f088e333f8400beba61333f643e76929859fbfa8
ariksieilavyj-ai <ariksieilavy@gmail.com>
2026-09-16 22:25:48 +0700

    Finish

--- app/src/main/java/com/example/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -34,6 +34,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    DemoScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -49,11 +50,13 @@ fun DemoText(message: String, fontSize: Float){
     )
 }
 
-@Preview
+@Preview(showSystemUi = true)
 @Composable
 fun DemoTextPreview(){
     ComposeDemoTheme{
-        DemoText(message = "Welcome to Android", fontSize = 12f)
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            DemoScreen(modifier = Modifier.padding(innerPadding))
+        }
     }
 }
 
@@ -76,7 +79,7 @@ fun DemoScreen(modifier: Modifier = Modifier) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()
+        modifier = modifier.fillMaxSize()
     ) {
         DemoText(message = "Welcome to Compose", fontSize = sliderPosition)
         Spacer(modifier = Modifier.height(150.dp))
